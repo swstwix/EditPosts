24b6052b911d714df2a4b7cbadd7b52f64d58fe8
unknown <f.demesh@demeshf.itransition.corp>
2012-07-25 12:37:36 +0300

    Add Services without models

---
+++ EditPosts.PresentationServices/Services/Concret/PostPresentationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EditPosts.PresentationServices.Services.Concret
+{
+    public class PostPresentationService : IPostPresentationService
+    {
+    }
+}
