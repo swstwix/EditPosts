6d0d435939d45a9bc069793056e3f7190b26c3a5
unknown <f.demesh@demeshf.itransition.corp>
2012-08-16 14:19:06 +0300

    _config to config

--- EditPosts.Db/DbConfig.cs
+++ EditPosts.Db/DbConfig.cs
@@ -8,22 +8,22 @@ namespace EditPosts.Db
 {
     public class DbConfig
     {
-        private static Configuration _config;
+        private static Configuration config;
 
         public static Configuration Configuration
         {
             get
             {
-                if (_config == null)
-                    _config =
+                if (config == null)
+                    config =
                         Fluently.Configure().Database(
                             MsSqlConfiguration.MsSql2008.ConnectionString(
-                                "Data Source=.\\SQLEXPRESS;AttachDbFilename=\"C:\\Users\\Twix\\Documents\\Visual Studio 2010\\Projects\\EditPosts\\EditPosts\\EditPosts.Views\\App_Data\\Database1.mdf\";Integrated Security=True;User Instance=True")).Mappings(
+                                "Data Source=.\\SQLEXPRESS;AttachDbFilename=\"C:\\Users\\f.demesh\\Documents\\Visual Studio 2010\\Projects\\EditPosts\\EditPosts.Views\\App_Data\\Database1.mdf\";Integrated Security=True;User Instance=True")).Mappings(
                                     m => m.FluentMappings.AddFromAssemblyOf<PostMap>()).
                             BuildConfiguration();
-                if (_config == null)
+                if (config == null)
                     throw new InvalidOperationException("NHibernate configuration is null");
-                return _config;
+                return config;
             }
             set { throw new NotImplementedException(); }
         }
---
+++ EditPosts.PresentationServices.Tests/ServicesTests/TagPresentationServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EditPosts.PresentationServices.Tests.ServicesTests
+{
+    [TestClass]
+    public class TagPresentationServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
